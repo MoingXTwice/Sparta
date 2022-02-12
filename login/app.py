4d08969f437ecb3f8f62287890359f17ac5272a6
MoingXTwice <sckbs1314@gmail.com>
2022-02-12 23:44:52 +0900

    login

--- login/app.py
+++ login/app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
+import pymongo
+
+# Database
+client = pymongo.MongoClient('localhost', 27017)
+db = client.user_login_system
 
 app = Flask(__name__)
+
+#Routes
+from user import routes
 
 @app.route('/')
 def home():
@@ -12,5 +20,6 @@ def dashboard():
 
 
 
-if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
+# run 파일로 돌리면 찾아지는 페에지들이 있는데 얘로 돌리면 안찾아진다 왜지?
+# if __name__ == '__main__':
+#    app.run('0.0.0.0', port=5000, debug=True)
