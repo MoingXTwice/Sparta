2a40240330987cf65853abc86e4e29e91863c3b3
MoingXTwice <sckbs1314@gmail.com>
2022-03-19 12:08:46 +0900

    2주차

--- spa_mall/app.js
+++ spa_mall/app.js
@@ -2,9 +2,19 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const goodsRouter = require('./routes/goods');
+
+const useMiddleWare = (req, res, next) => {
+    console.log("Request URL : ", req.originalUrl, " - ", new Date());
+    next();
+}
+
+app.use(useMiddleWare);
+app.use('/api', goodsRouter);
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
-})
+    res.send('hello world');
+});
 
 app.listen(port, () => {
     console.log(port, '포트로 서버가 켜졌어요!');
